f033e7bc35b73be81a0bafc54d70b37dc1aa95e4
ShaqUr <31959912+ShaqUr@users.noreply.github.com>
2017-10-10 19:14:41 +0200

    Add files via upload

---
+++ Copyfy/src/main/java/ShaqAndSoldier/Copyfy/CopyfyApplication.java
@@ -0,0 +1,12 @@
+package ShaqAndSoldier.Copyfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyfyApplication.class, args);
+	}
+}
