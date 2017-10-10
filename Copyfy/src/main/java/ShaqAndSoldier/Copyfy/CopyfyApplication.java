b06e66fb7405ebdb9bcb3da77c4acac8ffdbd7f0
shakkouraram@gmail.com <>
2017-10-10 19:41:17 +0200

    lmao BRO dis is a commit

--- Copyfy/src/main/java/ShaqAndSoldier/Copyfy/CopyfyApplication.java
+++ Copyfy/src/main/java/ShaqAndSoldier/Copyfy/CopyfyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CopyfyApplication {
 
 	public static void main(String[] args) {
-            System.out.println("fasz");
+            System.out.println("gecc");
 		SpringApplication.run(CopyfyApplication.class, args);
 	}
 }
