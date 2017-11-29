d7a64c1ba758adaf0e8364c6f322d90a48f67fc9
kjdavid <kjdavid96@gmail.com>
2017-11-29 17:17:11 +0100

    Login, regisztracios oldal megvan.

--- Copyfy/src/main/client/copfy-client/src/app/app.module.ts
+++ Copyfy/src/main/client/copfy-client/src/app/app.module.ts
@@ -8,16 +8,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from 'app/login.service';
+import { IndexComponent } from './index/index.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const appRoutes : Routes = [
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'index', component: IndexComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: '**', redirectTo: '/index'},
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    IndexComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
