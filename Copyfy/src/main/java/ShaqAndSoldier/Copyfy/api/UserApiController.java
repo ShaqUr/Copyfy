1f420dac00e5e91ba202dbfbc01839ca30d737b1
kjdavid <kjdavid96@gmail.com>
2017-12-01 09:11:37 +0100

    UserApiController added.

---
+++ Copyfy/src/main/java/ShaqAndSoldier/Copyfy/api/UserApiController.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ShaqAndSoldier.Copyfy.api;
+
+import ShaqAndSoldier.Copyfy.model.User;
+import static ShaqAndSoldier.Copyfy.model.User.Role.ADMIN;
+import static ShaqAndSoldier.Copyfy.model.User.Role.USER;
+import ShaqAndSoldier.Copyfy.service.UserService;
+import ShaqAndSoldier.Copyfy.service.annotations.Role;
+import ShaqAndSoldier.Copyfy.service.exceptions.UserNotValidException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserApiController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Role({USER, ADMIN})
+    @GetMapping
+    public ResponseEntity<User> user() {
+        if (userService.isLoggedIn()) {
+            return ResponseEntity.ok(userService.getUser());
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        try {
+            return ResponseEntity.ok(userService.login(user));
+        } catch (UserNotValidException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity logout(@RequestBody User user) {
+        this.userService.setUser(null);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        return ResponseEntity.ok(userService.register(user));
+    }
+}
